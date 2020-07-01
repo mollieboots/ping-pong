fb8b0f9996f961807fedbe01f5325eed881ed08c
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-06-30 19:23:52 -0700

    install jest and create tests folder

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { pingPong } from './ping-pong';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import $ from 'jquery';
 
 $(document).ready(function () {
     $('#ping-pong-form').submit(function (event) {
